403052d8832c981461345749561ea6e5771c4d35
Jian James Astrero <jianjamesastrero@gmail.com>
2020-12-03 04:39:30 +0800

    create Dimensions.kt to hold extension functions

---
+++ kdimens_extensions/src/main/java/com/jianastrero/kdimens_extensions/Dimensions.kt
@@ -0,0 +1,15 @@
+package com.jianastrero.kdimens_extensions
+
+import android.content.res.Resources
+
+val Number.dp: Float
+    get() = toFloat() * Resources.getSystem().displayMetrics.density
+
+val Number.pxOfDP: Float
+    get() = toFloat() / Resources.getSystem().displayMetrics.density
+
+val Number.sp: Number
+    get() = toFloat() * Resources.getSystem().displayMetrics.scaledDensity
+
+val Number.pxOfSP: Number
+    get() = toFloat() * Resources.getSystem().displayMetrics.scaledDensity
